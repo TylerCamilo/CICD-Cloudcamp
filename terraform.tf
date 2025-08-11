da71b019fcaf9c62278c1051ce266971d1d4c52c
Camilo_Carrillo <ccamilocubillos@gmail.com>
2025-08-11 13:05:50 -0500

    Terraform test cc

--- terraform.tf
+++ terraform.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "284698404434-camilo-carrillo"
+  bucket = "284698404434-camilo-carrillo-cubillos"
 
   tags = {
     Owner  = "camilocarrillo"
